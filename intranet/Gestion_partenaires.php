98be4fea91417b4dab31a755f9bd17500568c257
Amaury1350 <amaury.cournac@gmail.com>
2023-06-07 18:56:51 +0200

    Update gestion_partenaires.php

--- intranet/Gestion_partenaires.php
+++ intranet/Gestion_partenaires.php
@@ -7,12 +7,61 @@
 </head>
 <body>
 
-<?php navbar(3) ?>
+<?php navbar(3) ;
 
-<p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores necessitatibus numquam esse vel reprehenderit omnis labore mollitia, culpa dignissimos aliquam. Blanditiis obcaecati ipsum iure temporibus consequuntur mollitia sequi praesentium architecto?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, aliquid? Ab aspernatur dignissimos aliquid in laborum et labore nisi quaerat, neque distinctio iste voluptates optio rerum eius tempore? Reprehenderit, blanditiis.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam natus molestias esse iste, porro asperiores, quos architecto nemo delectus itaque inventore aspernatur tenetur maiores, temporibus sequi eius recusandae velit praesentium!
-</p>
+// Définition des partenaires
+$partenaires = array(
+  array(
+      'logo' => '../image/Mega-logo.png',
+      'nom' => 'Mega',
+      'description' => 'Mega est une marque dynamique et innovante dans le domaine des technologies de l\'information et de la communication. En tant que partenaire stratégique, notre entreprise a établi une relation de confiance avec Mega pour offrir à nos clients des solutions technologiques de premier ordre.',
+      'infos_privees' => 'Partenaire depuis le 02/09/2012 | Fournisseur de serveur pour le Cloud'
+  ),
+  array(
+      'logo' => '../image/HP-Logo.png',
+      'nom' => 'HP',
+      'description' => 'HP est une marque emblématique dans le domaine de la technologie, reconnue mondialement pour son expertise et ses innovations constantes. En tant que partenaire privilégié, notre entreprise a établi une solide collaboration avec HP afin de fournir à nos clients les meilleurs solutions.',
+      'infos_privees' => 'Partenaire depuis le 30/02/1999 | Fournisseur de PC et de laptop'
+  ),
+  array(
+    'logo' => '../image/Cisco-logo.png',
+    'nom' => 'Cisco',
+    'description' => 'Cisco est une marque leader dans le domaine des technologies de l\'information et de la communication, renommée pour ses solutions innovantes et sa capacité à transformer les entreprises du monde entier. En tant que partenaire de confiance, notre entreprise a établi une solide collaboration avec Cisco pour offrir à nos clients des solutions technologiques de pointe',
+    'infos_privees' => 'Partenaire depuis le 19/07/2002 | Fournisseur de routeur et switch'
+),
+  
+);
+
+// Fonction de test
+function partenaires($partenaire1) {
+
+    foreach ($partenaire1 as $partenaire) {
+      echo '<img src="' . $partenaire['logo'] . '" width="8%" alt="' . $partenaire['nom'] . '">';
+      echo '<h3>' . $partenaire1['nom'] . '</h3>';
+      echo '<p>' . $partenaire['description'] . '</p>';
+    }
+}
+
+// Encodage de la variable des partenaires en JSON
+$partenaires_json = json_encode($partenaires);
+
+// Stockage de la variable des partenaires dans un cookie
+setcookie('partenaires', $partenaires_json, time() + 3600); // Le cookie expire après 1 heure
+
+// Redirection vers la troisième page
+header("Location: Partenaires.php");
+
+
+
+?>
+
+<div class="container">
+    <?php   
+    echo '<div>'.table_info_part($partenaires).'</div>';
+
+    echo '<div>'.afficherPartenaires($partenaires).'</div>';
+    ?>
+</div>
 
 </body>
 </html>
