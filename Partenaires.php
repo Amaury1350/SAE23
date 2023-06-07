cfa35a74fd86b2bb0f23d8a46310fd261d5825a3
Amaury1350 <amaury.cournac@gmail.com>
2023-06-07 18:59:47 +0200

    Modification pour utiliser les fonctions et les nouvelles variable

--- Partenaires.php
+++ Partenaires.php
@@ -3,13 +3,23 @@
 <!doctype html>
 <html lang="en" data-bs-theme="auto">
 <head>
-  <?php include "fonctions.php";bootstrap("Site SAE23");?>
+  <?php include "fonctions.php";include "intranet/Gestion_partenaires.php";
+  bootstrap("Site SAE23");
+  
+  
+  
+  ?>
 </head>
 <body>
   <div class="principal">
     <?php nav("image/logo3.png",5); ?>
     <div class="container mt-4">
-      <p>test</p>
+      <?php 
+    $partenaires_json = $_COOKIE['partenaires'];
+    $partenaires = json_decode($partenaires_json, true);
+  
+    afficherPartenaires($partenaires);
+    ?>
     </div>
   </div>
 
@@ -20,5 +30,3 @@
   </div>       
 </body>
 </html>
-
-
