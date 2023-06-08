d9db35c65482d07f9df83a07036630b7c41d332d
Antoine Cadiou <antoine.cadiou@etudiant.univ-rennes1.fr>
2023-06-08 15:01:25 +0200

    ajout des photos de profil de chaque employé

--- intranet/annuaire.php
+++ intranet/annuaire.php
@@ -24,45 +24,45 @@
         <?php
             // Données de l/'annuaire
             $annuaire = array(
-                array("DUHAMEL", "Louis", "Technicien", createID("Louis","DUHAMEL"), "Administrateur"),
-                array("PAUL", "Pierre", "DRH",createID("Pierre","PAUL"), "Direction"),
-                array("MARSEAU", "François", "Directeur",createID("François","MARSEAU"), "Direction"),
-                array("BOISVERT", "Alita", "Hot liner", createID("Alita","BOISVERT"), "Salarié"),
-                array("BOSSE", "André", "Administrateur réseaux", createID("André","BOSSE"), "Administrateur"),
-                array("MAINVILLE", "André", "Ingénieur cloud computing",createID("André","MAINVILLE"), "Manager"),
-                array("BEAUPRE", "Christabel", "Administrateur système",createID("Christabel","BEAUPRE"), "Administrateur"),
-                array("LOUIS", "Patricia", "Ingénieure cloud computing", createID("Patricia","LOUIS"), "Salarié"),
-                array("MOTHE", "Geneviève", "Technicien", createID("Geneviève","MOTHE"), "Salarié"),
-                array("SANSOUCI", "Auda", "Développeur web",createID("Auda","SANSOUCI"), "Salarié"),
-                array("MOREL", "Gilles", "Programmeur",createID("Gilles","MOREL"), "Salarié"),
-                array("RIQUIER", "Julien", "Technicien", createID("Julien","RIQUIER"), "Salarié"),
-                array("CADIEUX", "Lea", "Chef de projet", createID("Lea","CADIEUX"), "Direction"),
-                array("SEVIER", "Valentin", "Architecte logiciel",createID("Valentin","SEVIER"), "Direction"),
-                array("MERCIER", "Suzette", "Programmeur",createID("Suzette","MERCIER"), "Manager"),
-                array("COURTLAND", "Elene", "Technicien", createID("Elene","COURTLAND"), "Salarié"),
-                array("TARDIF", "Céline", "Développeur web", createID("Céline","TARDIF"), "Administrateur"),
-                array("HACHE", "Carole", "Architecte logiciel",createID("Carole","HACHE"), "Salarié"),
-                array("GUAY", "Madeleine", "Architecte cloud",createID("Madeleine","GUAY"), "Salarié"),
-                array("METHOT", "Elodie", "Technicien", createID("Elodie","METHOT"), "Salarié"),
-                array("PAIEN", "Raymond", "Ingénieur cloud computing", createID("Raymond","PAIEN"), "Administrateur"),
-                array("RANCOURT", "Nicolas", "Architecte cloud",createID("Nicolas","RANCOURT"), "Salarié"),
-                array("DESRUISSEAUX", "Astrid", "Hot liner",createID("Astrid","DESRUISSEAUX"), "Salarié"),
-                array("AUGER", "Olivier", "Technicien", createID("Olivier","AUGER"), "Salarié"),
-                array("PICARD", "Emmanuelle", "Responsable de la sécurité des systèmes d'information", createID("Emmanuelle","PICARD"), "Administrateur"),
-                array("AUPRY", "Maurelle", "Hot liner",createID("Maurelle","AUPRY"), "Salarié"),
-                array("DROUIN", "Matthieu", "Directeur technique",createID("Matthieu","DROUIN"), "Direction"),
-                array("BERARD", "Charline", "Responsable informatique", createID("Charline","BERARD"), "Manager"),
-                array("GAUDREAU", "Claire", "Comptable", createID("Claire","GAUDREAU"), "Salarié"),
-                array("MERCIER", "Pauline", "Technicien",createID("Pauline","MERCIER"), "Salarié"),
-                array("ARTOIS", "Claude", "Comptable",createID("Claude","ARTOIS"), "Salarié"),
+                array("image/profil1.jpg", "DUHAMEL", "Louis", "Technicien", createID("Louis","DUHAMEL"), "Administrateur"),
+                array("image/profil2.jpg", "PAUL", "Pierre", "DRH",createID("Pierre","PAUL"), "Direction"),
+                array("image/profil5.jpg", "MARSEAU", "François", "Directeur",createID("François","MARSEAU"), "Direction"),
+                array("image/profil16.jpg", "BOISVERT", "Alita", "Hot liner", createID("Alita","BOISVERT"), "Salarié"),
+                array("image/profil3.jpg", "BOSSE", "André", "Administrateur réseaux", createID("André","BOSSE"), "Administrateur"),
+                array("image/profil4.jpg", "MAINVILLE", "André", "Ingénieur cloud computing",createID("André","MAINVILLE"), "Manager"),
+                array("image/profil17.jpg", "BEAUPRE", "Christabel", "Administrateur système",createID("Christabel","BEAUPRE"), "Administrateur"),
+                array("image/profil18.jpg", "LOUIS", "Patricia", "Ingénieure cloud computing", createID("Patricia","LOUIS"), "Salarié"),
+                array("image/profil19.jpg", "MOTHE", "Geneviève", "Technicien", createID("Geneviève","MOTHE"), "Salarié"),
+                array("image/profil20.jpg", "SANSOUCI", "Auda", "Développeur web",createID("Auda","SANSOUCI"), "Salarié"),
+                array("image/profil10.jpg", "MOREL", "Gilles", "Programmeur",createID("Gilles","MOREL"), "Salarié"),
+                array("image/profil6.jpg", "RIQUIER", "Julien", "Technicien", createID("Julien","RIQUIER"), "Salarié"),
+                array("image/profil21.jpg", "CADIEUX", "Lea", "Chef de projet", createID("Lea","CADIEUX"), "Direction"),
+                array("image/profil7.jpg", "SEVIER", "Valentin", "Architecte logiciel",createID("Valentin","SEVIER"), "Direction"),
+                array("image/profil22.jpg", "MERCIER", "Suzette", "Programmeur",createID("Suzette","MERCIER"), "Manager"),
+                array("image/profil23.jpg", "COURTLAND", "Elene", "Technicien", createID("Elene","COURTLAND"), "Salarié"),
+                array("image/profil24.jpg", "TARDIF", "Céline", "Développeur web", createID("Céline","TARDIF"), "Administrateur"),
+                array("image/profil25.jpg", "HACHE", "Carole", "Architecte logiciel",createID("Carole","HACHE"), "Salarié"),
+                array("image/profil26.jpg", "GUAY", "Madeleine", "Architecte cloud",createID("Madeleine","GUAY"), "Salarié"),
+                array("image/profil27.jpg", "METHOT", "Elodie", "Technicien", createID("Elodie","METHOT"), "Salarié"),
+                array("image/profil8.jpg", "PAIEN", "Raymond", "Ingénieur cloud computing", createID("Raymond","PAIEN"), "Administrateur"),
+                array("image/profil9.jpg", "RANCOURT", "Nicolas", "Architecte cloud",createID("Nicolas","RANCOURT"), "Salarié"),
+                array("image/profil28.jpg", "DESRUISSEAUX", "Astrid", "Hot liner",createID("Astrid","DESRUISSEAUX"), "Salarié"),
+                array("image/profil11.jpg", "AUGER", "Olivier", "Technicien", createID("Olivier","AUGER"), "Salarié"),
+                array("image/profil12.jpg", "PICARD", "Emmanuel", "Responsable de la sécurité des systèmes d'information", createID("Emmanuelle","PICARD"), "Administrateur"),
+                array("image/profil29.jpg", "AUPRY", "Maurelle", "Hot liner",createID("Maurelle","AUPRY"), "Salarié"),
+                array("image/profil13.jpg", "DROUIN", "Matthieu", "Directeur technique",createID("Matthieu","DROUIN"), "Direction"),
+                array("image/profil14.jpg", "BERARD", "Bernard", "Responsable informatique", createID("Charline","BERARD"), "Manager"),
+                array("image/profil30.jpg", "GAUDREAU", "Claire", "Comptable", createID("Claire","GAUDREAU"), "Salarié"),
+                array("image/profil15.jpg", "ARTOIS", "Claude", "Comptable",createID("Claude","ARTOIS"), "Salarié"),
             );
+
+            // Affichage du tableau
+            echo "<table class='table table-striped table-hover table-bordered'>";
+            echo "<thead><tr><th>Photo de profil</th><th>Nom</th><th>Prénom</th><th>Poste</th><th>Identifiant</th><th>Groupe</th></tr></thead>";
             $json=json_encode($annuaire);
             $file_path='annuaire.json';
             $_SESSION['Identifiant']=$annuaire;
             file_put_contents($file_path, $annuaire_json);
-            // Affichage du tableau
-            echo "<table class='table table-striped table-hover table-bordered'>";
-            echo "<thead><tr><th>Nom</th><th>Prénom</th><th>Poste</th><th>Identifiant</th><th>Groupe</th></tr></thead>";
 
             foreach ($annuaire as $colonne) {
                 echo "<tr>";
