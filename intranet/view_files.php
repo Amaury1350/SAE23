167ac3639b499c4913cb9362acdd4102b5840060
Antoine Leray <antoine.leray@etudiant.univ-rennes1.fr>
2023-06-08 15:34:08 +0200

    modif visualisation de fichier

--- intranet/view_files.php
+++ intranet/view_files.php
@@ -7,18 +7,63 @@
   <title>Liste des fichiers</title>
 </head>
 <body>
-  <h2>Liste des fichiers</h2>
+<?php navbar(2) ?>
+<div class="container mt-4">
+        <div class="pt-5 my-2 text-center">
+            <h1 style="color:22333b;" class="display-4 fw-bold text-body-emphasis mx-4">Gestionnaire de fichiers</h1>
+            <div class="col-lg-6 mx-auto">
+                <h4 class="lead mb-4">Voici la liste des fichiers</h4>
+                <br>
+            </div>
+        </div>  
+
   <?php
   $uploadsDir = '../Fichiers/';
   $files = scandir($uploadsDir);
   foreach ($files as $file) {
     if ($file != '.' && $file != '..') {
-      echo '<a href="' . $uploadsDir . $file . '" download>' . $file . '</a><br>';
+      echo '<div class="d-flex justify-content-between align-items-center">
+                <a href="' . $uploadsDir . $file . '" download>' . $file . '</a>
+                <form action="supprimer_fichiers.php" method="post" style="display: inline;">
+                    <input type="hidden" name="filename" value="' . $file . '">
+                    <button type="submit" class="btn btn-danger">Supprimer</button>
+                </form>
+            </div><br>';
     }
   }
+
+
+
+  // a voir
   ?>
+  <script>
+        function supprimerFichier() {
+            var filename = document.getElementById("filename").value;
+            var uploadsDir = '../Fichiers/';
+
+            // Créer une requête XMLHttpRequest
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'supprimer_fichier.php', true);
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+            // Callback de la requête
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    // Afficher la réponse
+                    alert(xhr.responseText);
+
+                    // Actualiser la page
+                    window.location.reload();
+                }
+            };
+
+            // Envoyer la requête avec les données du formulaire
+            xhr.send('filename=' + filename);
+        }
+    </script>
   <div class="d-flex justify-content-center align-items-center">
-    <a href="Gestion_Fichiers.php" class="btn btn-warning">Cliquez ici pour revenir à la page prédédente</a>
+    <a href="Gestion_Fichiers.php" class="btn btn-warning">Cliquez ici pour revenir à la page précédente</a>
   </div>
+</div>
 </body>
 </html>
