625b5f3a7943b886d2ac34119b8c11020d2997b3
Damien Chesnot <damien.chesnot@etudiant.univ-rennes1.fr>
2023-06-09 14:57:08 +0200

    sess

--- intranet/connexion.php
+++ intranet/connexion.php
@@ -1,9 +1,6 @@
-<?php session_start(); // Création de la session ?>
-
-<!doctype html>
-<html lang="fr">
-<body>
 <?php
+session_start(); // Création de la session
+
 $file_path = 'annuaire.json';
 $json_data = file_get_contents($file_path);
 $data = json_decode($json_data, true);
@@ -13,35 +10,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $motdepasse_saisi = $_POST['motdepasse'];
 
     // Vérifier l'authentification de l'utilisateur
-           
-           
-            foreach ($data as $colonne) {
-            if($utilisateur_saisi == $colonne[4]){
-                if ($motdepasse_saisi == 'bonjour'){
-                    $_SESSION['prenom'] = $_POST['utilisateur'];
-                    header("Location: Accueil.php");
-                    exit();
-                    echo 'Authentification réussie';
-                    echo ('Utilisateur: '.$_SESSION['prenom'].'<hr>');
+    foreach ($data as $colonne) {
+        if ($utilisateur_saisi == $colonne[4]){
+                if ($motdepasse_saisi == 'bonjour') {
+                $_SESSION['prenom'] = $_POST['utilisateur'];
+                header("Location: Accueil.php");
+                exit();
                 } else {
-                    // Authentification échouée
-                    echo 'Mot de passe incorrect';
+               
+                echo 'Mot de passe incorrect';
                 }
-            } else {
-                echo 'Utilisateur inconnue';  
-            }
-
-            } 
-
-    // Vérifier le mot de passe
+        } else{
+            echo 'Utilisateur inconnu';
+        //header("Location: ../page_sign_in.php");
+        //exit();
+        }
+    }
+    $users=$_SESSION['groupe'];
+    var_dump($users);
+    
+    // Si la boucle foreach termine sans authentification réussie
+    
 }
 
 // Vérifier si un utilisateur est connecté
-if (isset($_SESSION['prenom'])){
-    echo 'Utilisateur connecté: ' . $_SESSION['prenom'];
-} else {
-    echo 'Utilisateur non connecté';
-}
+//if (isset($_SESSION['prenom'])) {
+//    echo 'Utilisateur connecté: ' . $_SESSION['prenom'];
+//} else {
+//    echo 'Utilisateur non connecté';
+//}
 ?>
-</body>
-</html>
